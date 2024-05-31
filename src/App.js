5f3711fead381683baa9b0de0e908ca2a1300a00
geeann <geeann.ibon@gmail.com>
2024-05-31 14:41:16 +0800

    fix warning about image alt text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div class="app">
-      <img class="profile" src={pfp}></img>
+      <img class="profile" src={pfp} alt='anime version of IU'></img>
       <h1>Gee Ann</h1>
       <p>......</p>
     </div>
